c0dfdb834f1a75fe41b9764a2787cf9139afb7cc
Sergey Mamyan <sergey.mamyan@gmail.com>
2015-12-10 20:11:14 +0400

    Committing Changes

---
+++ url.ts
@@ -0,0 +1,18 @@
+export interface Url {
+    href?: string;
+    protocol?: string;
+    auth?: string;
+    hostname?: string;
+    port?: string;
+    host?: string;
+    pathname?: string;
+    search?: string;
+    query?: any; // string | Object
+    slashes?: boolean;
+    hash?: string;
+    path?: string;
+}
+
+export declare function parse(urlStr: string, parseQueryString?: boolean , slashesDenoteHost?: boolean ): Url;
+export declare function format(url: Url): string;
+export declare function resolve(from: string, to: string): string;
