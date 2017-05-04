2d07a02540f8b4f6dfd196f8e5dcfdeaf21478f2
Grigor <ggrigoryan@bills.com>
2017-05-04 20:49:33 +0400

    adding missed module registration

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 System.register("@ecmal/node/buffer",[],function(exports,context){
     context.exports = require('buffer');
 })
-System.register("@ecmal/node/path",[],function(exports,context){
-    context.exports = require('path')
+System.register("@ecmal/node/child_process",[],function(exports,context){
+    context.exports = require('child_process')
 })
-System.register("@ecmal/node/fs",[],function(exports,context){
-    context.exports = require('fs')
+System.register("@ecmal/node/cluster",[],function(exports,context){
+    context.exports = require('cluster')
+})
+System.register("@ecmal/node/console",[],function(exports,context){
+    context.exports = require('console')
 })
 System.register("@ecmal/node/crypto",[],function(exports,context){
     context.exports = require('crypto')
 })
-System.register("@ecmal/node/url",[],function(exports,context){
-    context.exports = require('url')
+System.register("@ecmal/node/dgram",[],function(exports,context){
+    context.exports = require('dgram')
 })
-System.register("@ecmal/node/querystring",[],function(exports,context){
-    context.exports = require('querystring')
+System.register("@ecmal/node/dns",[],function(exports,context){
+    context.exports = require('dns')
+})
+System.register("@ecmal/node/events",[],function(exports,context){
+    context.exports = require('events')
+})
+System.register("@ecmal/node/fs",[],function(exports,context){
+    context.exports = require('fs')
 })
 System.register("@ecmal/node/http",[],function(exports,context){
     context.exports = require('http')
 })
 System.register("@ecmal/node/https",[],function(exports,context){
     context.exports = require('https')
+})
+System.register("@ecmal/node/net",[],function(exports,context){
+    context.exports = require('net')
+})
+System.register("@ecmal/node/os",[],function(exports,context){
+    context.exports = require('os')
+})
+System.register("@ecmal/node/path",[],function(exports,context){
+    context.exports = require('path')
+})
+System.register("@ecmal/node/punycode",[],function(exports,context){
+    context.exports = require('punycode')
+})
+System.register("@ecmal/node/querystring",[],function(exports,context){
+    context.exports = require('querystring')
+})
+System.register("@ecmal/node/readline",[],function(exports,context){
+    context.exports = require('readline')
+})
+System.register("@ecmal/node/repl",[],function(exports,context){
+    context.exports = require('repl')
+})
+System.register("@ecmal/node/stream",[],function(exports,context){
+    context.exports = require('stream')
+})
+System.register("@ecmal/node/string_decoder",[],function(exports,context){
+    context.exports = require('string_decoder')
+})
+System.register("@ecmal/node/tls",[],function(exports,context){
+    context.exports = require('tls')
+})
+System.register("@ecmal/node/tty",[],function(exports,context){
+    context.exports = require('tty')
+})
+System.register("@ecmal/node/url",[],function(exports,context){
+    context.exports = require('url')
+})
+System.register("@ecmal/node/util",[],function(exports,context){
+    context.exports = require('util')
+})
+System.register("@ecmal/node/v8",[],function(exports,context){
+    context.exports = require('v8')
+})
+System.register("@ecmal/node/vm",[],function(exports,context){
+    context.exports = require('vm')
+})
+System.register("@ecmal/node/zlib",[],function(exports,context){
+    context.exports = require('zlib')
 })
